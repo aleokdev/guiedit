156406bbce7573b0ac561a835f3094e144f462fb
alexdevteam <alexpro820@gmail.com>
2022-10-30 13:55:43 +0100

    Add inspectable for Textures, SfBox, SoundBuffer

--- src/sfml/inspectable.rs
+++ src/sfml/inspectable.rs
@@ -1,6 +1,10 @@
+use std::ops::DerefMut;
+
 use sfml::{
-    graphics::Color,
+    audio::SoundBuffer,
+    graphics::{Color, Texture},
     system::{Vector2, Vector3},
+    SfBox,
 };
 
 use crate::inspectable::Inspectable;
@@ -35,5 +39,30 @@ impl<T: egui::emath::Numeric> Inspectable for Vector3<T> {
                 ui.add(egui::DragValue::new(&mut self.z));
             })
         });
+    }
+}
+
+impl Inspectable for SoundBuffer {
+    fn inspect_ui(&mut self, ui: &mut egui::Ui) {
+        // TODO: Better impl
+        ui.label(format!(
+            "SoundBuffer; {:.1}s long",
+            self.duration().as_seconds()
+        ));
+    }
+}
+
+impl<T: Inspectable + sfml::SfResource> Inspectable for SfBox<T> {
+    fn inspect_ui(&mut self, ui: &mut egui::Ui) {
+        self.deref_mut().inspect_ui(ui);
+    }
+}
+
+impl Inspectable for Texture {
+    fn inspect_ui(&mut self, ui: &mut egui::Ui) {
+        ui.image(
+            egui::TextureId::User(self.native_handle() as u64),
+            egui::Vec2::new(self.size().x as f32, self.size().y as f32),
+        );
     }
 }
